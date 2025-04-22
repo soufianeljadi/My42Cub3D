88804a71c4ee25d2761b85657c01e72c1bf6e8ea
Ahmad Ben Hssain <aben-hss@e1r7p1.1337.ma>
2025-04-22 15:07:05 +0100

    update header and Makefile

--- Makefile
+++ Makefile
@@ -1,13 +1,13 @@
 NAME = cub3D
 
-SRCS = src/main.c src/cast_rays.c src/parser.c \
-		gnl/gnl.c gnl/gnl_utils.c \
+SRCS = src/main.c src/cast_rays.c src/draw.c src/move.c src/parser.c \
+		src/distance.c gnl/gnl.c gnl/gnl_utils.c \
 		utils/ft_atoi.c utils/ft_strchr.c utils/ft_strcmp.c utils/ft_strncmp.c utils/ft_strjoin.c utils/ft_strdup.c \
 		utils/ft_strtok.c utils/ft_substr.c utils/ft_strrchr.c utils/ft_strtrim.c utils/ft_isdigit.c utils/ft_split.c 
 OBJS = $(SRCS:.c=.o)
 
 CC = cc
-CFLAGS =-fsanitize=address -g3 #-Wall -Wextra -Werror 
+# CFLAGS =-fsanitize=address -g3 #-Wall -Wextra -Werror 
 MLX42= MLX42/libmlx42.a
 MLXFLAGS = -Iinclude -ldl -lglfw -pthread -lm
 
--- includes/cub3d.h
+++ includes/cub3d.h
@@ -16,44 +16,44 @@
 #define FOV (M_PI/ 3)
 
 typedef struct s_data {
-    char *north_texture;
-    char *south_texture;
-    char *west_texture;
-    char *east_texture;
-    int floor_color[3];
-    int ceiling_color[3];
-    char **map;
-    int map_width;
-    int map_height;
-    int map_capacity;
-    int player_x;
-    int player_y;
-    char player_dir;
-    int has_north_texture;
-    int has_south_texture;
-    int has_west_texture;
-    int has_east_texture;
-    int has_floor_color;
-    int has_ceiling_color;
+	char *north_texture;
+	char *south_texture;
+	char *west_texture;
+	char *east_texture;
+	int floor_color[3];
+	int ceiling_color[3];
+	char **map;
+	int map_width;
+	int map_height;
+	int map_capacity;
+	int player_x;
+	int player_y;
+	char player_dir;
+	int has_north_texture;
+	int has_south_texture;
+	int has_west_texture;
+	int has_east_texture;
+	int has_floor_color;
+	int has_ceiling_color;
 } t_data;
 
 typedef struct s_player {
-    double x;
-    double y;
-    double dir;
+	double x;
+	double y;
+	double dir;
 } t_player;
 
 typedef struct s_params{
-    int wall_start;
-    int wall_end;
-    int wall_height;
+	int wall_start;
+	int wall_end;
+	int wall_height;
 	mlx_t *mlx;
 	char **map;
 	mlx_image_t *img;
-    mlx_texture_t   *south_t;
-    mlx_texture_t   *west_t;
-    mlx_texture_t   *east_t;
-    mlx_texture_t   *north_t;
+	mlx_texture_t   *south_t;
+	mlx_texture_t   *west_t;
+	mlx_texture_t   *east_t;
+	mlx_texture_t   *north_t;
 	t_player player;
 	t_data data;
 	int height;
@@ -62,14 +62,14 @@ typedef struct s_params{
 
 
 typedef struct s_ray {
-    double angle;
-    double ver_dis;
-    double hor_dis;
-    double distance;
-    int map_x;
-    int map_y;
-    int facing_up;
-    int facing_right;
+	double angle;
+	double ver_dis;
+	double hor_dis;
+	double distance;
+	int map_x;
+	int map_y;
+	int facing_up;
+	int facing_right;
 } t_ray;
 
 typedef struct s_wall_data
@@ -89,6 +89,15 @@ t_wall_data		wall;
 mlx_texture_t	*text;
 float			text_x;
 }	t_draw_data;
+
+typedef struct s_intersection {
+    double a_y;
+    double a_x;
+    double y_step;
+    double x_step;
+    int map_y;
+    int map_x;
+} t_intersection;
 
 void parse_cub_file(t_data *data, const char *filename);
 void parse_texture(t_data *data, char *line);
@@ -111,6 +120,30 @@ int		ft_search(char *s, int c);
 char	*ft_move_start(char *start);
 char	*ft_strjoin_gnl(char *start, char *buff);
 char	*ft_join(char *dest, char *s1, char *s2);
+
+// render
+void	player_rotation(t_params *params);
+void	calc_new_pos(t_params *params, double a_off, double *t_x, double *t_y);
+void	forward_backward(t_params *params);
+void	move_sideways(t_params *params);
+void	player_movement(t_params *params);
+void	get_hors_inter(t_params *par, t_ray *ray);
+void	get_verts_inter(t_params *par, t_ray *ray);
+void	set_to_pos(t_params *params, double x, double y);
+void	key_hook(void *data);
+void	handle_flip_texture(t_draw_data *data);
+void	draw_ceiling(t_draw_data *data);
+void	draw_wall_column(t_draw_data *data);
+void	draw_floor(t_draw_data *data);
+void	draw_wall(t_params *params, t_ray ray, mlx_image_t *img, int x);
+void	set_position(t_player *player, char **map, t_data *data);
+void	init_draw_data(t_draw_data *data, t_params *params, t_ray ray, \
+	mlx_image_t *img, t_wall_data wall);
+void	calculate_texture_coordinates(t_draw_data *data);
+void	cast_rays(t_params *params, mlx_image_t *img);
+void	cast_player(t_params *params);
+
+
 
 //utils
 char    *ft_strtok(char *str, const char *delim);
